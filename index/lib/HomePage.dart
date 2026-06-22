742ca706637dc82d141f4da320985e9ecad943cc
picheng5555kh-arch <picheng5555kh@gmail.com>
2026-06-22 14:59:18 +0700

    uppdat file

--- index/lib/HomePage.dart
+++ index/lib/HomePage.dart
@@ -17,7 +17,7 @@ import 'package:index/main.dart';
        body:Center(
          child: Column(
           children: [
-            Image.network("https://justsiemreap.com/wp-content/uploads/2018/05/Angkor-Wat-temple-guide-2.jpg"),
+            //Image.network("https://justsiemreap.com/wp-content/uploads/2018/05/Angkor-Wat-temple-guide-2.jpg"),
             
             Image.network("https://cdn.adventure-life.com/81/11/8/5qg25p24/1300x820.webp"),
             Image.network("https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2f/6e/59/34/caption.jpg?w=900&h=500&s=1"),
